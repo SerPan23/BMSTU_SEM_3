08d32d2361043f9b2ee871e9bce30ddfde00e318
Sergei <igracom@yandex.ru>
2023-09-08 23:28:39 +0300

    second version without full tests and rounding

--- TASD/lab_01/bignum.c
+++ TASD/lab_01/bignum.c
@@ -1,11 +1,29 @@
 #include "bignum.h"
 
+int normalize_zeros(bignum_t *a)
+{
+    ptrdiff_t start_ind = a->mantissa_len - 1;
+    for (ptrdiff_t i = start_ind; i >= 0; i--)
+        if (a->mantissa[i] == 0)
+        {
+            a->mantissa_len--;
+            a->exponet--;
+        }
+    return EXIT_SUCCESS;
+}
+
 int bignum_print(bignum_t *a)
 {
+    int rc = normalize_zeros(a);
+    if (rc != EXIT_SUCCESS)
+        return rc;
+    
     char s[MAX_STR_LEN + 1];
     size_t l;
 
-    bignum_to_str(s, &l, a);
+    rc = bignum_to_str(s, &l, a);
+    if (rc != EXIT_SUCCESS)
+        return rc;
     printf("%s\n", s);
     return EXIT_SUCCESS;
 }
@@ -24,36 +42,94 @@ int bignum_scan(bignum_t *a)
    return rc;
 }
 
+int find_exponet(char str[], size_t len, int32_t *exponet, ptrdiff_t *e_ind)
+{
+    char *p;
+    p = strstr(str, "E");
+    if (p != NULL)
+    {
+        *e_ind = p - str;
+        ptrdiff_t pt = *e_ind;
+
+        int is_sign = 0;
+        if (str[*e_ind + 1] == '-' || str[*e_ind + 1] == '+')
+            is_sign = 1;
+        
+        if ((len - 1 - (*e_ind) > 5) || (is_sign && (len - 1 - (*e_ind) > 6)))
+            return ERROR_EXPONET_SIZE;
+        
+        if (is_sign)
+            pt++;
+
+        int tmp = 0;
+        for (size_t i = pt + 1; i < len; i++)
+        {
+            if (!isdigit(str[i]))
+                return ERROR_INVALID_SYMBOL;
+            tmp *= 10;
+            tmp += (int)(str[i] - '0');
+        }
+        *exponet = tmp;
+        if (str[*e_ind + 1] == '-')
+            *exponet *= -1;
+    }
+    return EXIT_SUCCESS;
+}
+
+int find_no_zero_digit(char str[], size_t len, ptrdiff_t *no_0_ind)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        if (isdigit(str[i]) && str[i] != '0')
+        {
+            *no_0_ind = i;
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+int find_dot(char str[], size_t len, ptrdiff_t *dot_ind)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+         if (str[i] == '.')
+        {
+            *dot_ind = i;
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
 int str_to_bignum(char s[], size_t len, bignum_t *a)
 {
     a->is_negative = 0;
     a->mantissa_len = 0;
     a->exponet = 0;
-    int is_dot = 0, is_E = 0;
-    for (ptrdiff_t i = len - 1; i >= 0; i--)
-    {
-        if ( ((s[i] == '-' || s[i] == '+') && s[i - 1] == 'E' && !is_E) || (s[i] == 'E' && !is_E))
-        {
-            if (a->mantissa_len > 5)
-                return ERROR_EXPONET_SIZE;
-            else
-            {
-                int tmp = 0;
-                for (ptrdiff_t j = a->mantissa_len - 1; j >= 0; j--)
-                {
-                    tmp *= 10;
-                    tmp += a->mantissa[j];
-                }
-                a->exponet = tmp;
-                if (s[i] == '-')
-                    a->exponet *= -1;
-                a->mantissa_len = 0;
-            }
-            is_E = 1;
-            if (s[i] == '-' || s[i] == '+')
-                i--;
-        }
-        else if (i == 0 && !isdigit(s[i]))
+
+    ptrdiff_t e_ind = len, no_0_ind = 0, dot_ind = -1;
+
+    int rc = find_exponet(s, len, &(a->exponet), &e_ind);
+
+    if (rc != EXIT_SUCCESS)
+        return rc;
+
+    rc = find_no_zero_digit(s, len, &no_0_ind);
+
+    if (rc != EXIT_SUCCESS)
+        return rc;
+
+    rc = find_dot(s, len, &dot_ind);
+
+    if (rc != EXIT_SUCCESS)
+        return rc;
+
+    int is_start_num = 0;
+
+    for (ptrdiff_t i = e_ind - 1; i >= 0; i--)
+    {   
+        if (i == 0 && !isdigit(s[i]))
         {
             if (s[i] == '-')
                 a->is_negative = 1;
@@ -62,25 +138,36 @@ int str_to_bignum(char s[], size_t len, bignum_t *a)
         }
         else if (isdigit(s[i]))
         {
-            int digit = (int)(s[i] - '0');
-            if (digit == 0 && (i == 0 || (i == 1 && (s[i - 1] == '-' || s[i - 1] == '+'))))
-                continue;
-            a->mantissa[a->mantissa_len++] = digit;
-            if (is_dot)
-                a->exponet++;
+            if (i >= no_0_ind)
+            {    
+                int digit = (int)(s[i] - '0');
+                // if (digit == 0 && (i == 0 || (i == 1 && (s[i - 1] == '-' || s[i - 1] == '+'))))
+                //     continue;
+
+                if (!is_start_num)
+                {
+                    if (digit)
+                        is_start_num = 1;
+                }
+
+                if (is_start_num)
+                    a->mantissa[a->mantissa_len++] = digit;
+                if ((dot_ind != -1 && i < dot_ind) || dot_ind == -1)
+                    a->exponet++;
+            }
         }
         else if (s[i] == '.')
         {
-            if (!is_dot)
-                is_dot = 1;
-            else
+            if (dot_ind != -1 && i != dot_ind)
                 return ERROR_INVALID_SYMBOL;
+            else
+                continue;
         }
         else
             return ERROR_INVALID_SYMBOL;
     }
-    if (!is_dot)
-         a->exponet += a->mantissa_len;
+    // if (dot_ind == -1)
+    //     a->exponet += a->mantissa_len;
     return EXIT_SUCCESS;
 }
 int bignum_to_str(char s[], size_t *len, bignum_t *a)
@@ -116,7 +203,7 @@ int bignum_mul(bignum_t *a, bignum_t *b, bignum_t *result)
         tmp[i] = 0;
 
     result->exponet = a->exponet + b->exponet;
-    result->is_negative = a->is_negative * b->is_negative;
+    result->is_negative = (a->is_negative + b->is_negative) % 2;
 
     size_t i, j;
     for (i = 0; i < b->mantissa_len; i++)
--- TASD/lab_01/main.c
+++ TASD/lab_01/main.c
@@ -1,5 +1,16 @@
 #include "bignum.h"
 
+#define MAX_FIRST_MANTISSA_LEN 35
+#define MAX_SECOND_MANTISSA_LEN 40
+#define MAX_RESULT_MANTISSA_LEN 40
+
+#define MAX_RESULT_EXPONET_SIZE 99999
+
+#define ERROR_FIRST_MANTISSA_LEN 10
+#define ERROR_SECOND_MANTISSA_LEN 20
+#define ERROR_RESULT_MANTISSA_LEN 30
+#define ERROR_RESULT_EXPONET_SIZE 40
+
 void debug_print(bignum_t *a)
 {
     printf("\nDebug----------------------------\n");
@@ -17,7 +28,7 @@ void debug_print(bignum_t *a)
 int main(void)
 {
     bignum_t a;
-    printf("Enter first num:\n----|----|----|----|----|----|----|\n");
+    printf("Enter first num:\n----1----2----3----4----5----6----7\n");
     int rc = bignum_scan(&a);
     if (rc != EXIT_SUCCESS)
     {
@@ -25,8 +36,11 @@ int main(void)
         return rc;
     }
 
+    if (a.mantissa_len > MAX_FIRST_MANTISSA_LEN)
+        return ERROR_FIRST_MANTISSA_LEN;
+
     bignum_t b;
-    printf("Enter second num:\n----|----|----|----|----|----|----|----|\n");
+    printf("Enter second num:\n----1----2----3----4----5----6----7----8\n");
     rc = bignum_scan(&b);
     if (rc != EXIT_SUCCESS)
     {
@@ -34,17 +48,30 @@ int main(void)
         return rc;
     }
 
+    if (b.mantissa_len > MAX_SECOND_MANTISSA_LEN)
+        return ERROR_SECOND_MANTISSA_LEN;
 
-    debug_print(&a);
+    //10
+    //22
+    // 0.0220E4
+    //00000.01000
+
+    // debug_print(&a);
     bignum_print(&a);
-    debug_print(&b);
+    // debug_print(&b);
     bignum_print(&b);
 
     bignum_t res;
 
     bignum_mul(&a, &b, &res);
 
-    debug_print(&res);
+    if (res.mantissa_len > MAX_RESULT_MANTISSA_LEN)
+        return ERROR_RESULT_MANTISSA_LEN;
+
+    if (res.mantissa_len > MAX_RESULT_EXPONET_SIZE)
+        return ERROR_RESULT_EXPONET_SIZE;
+
+    // debug_print(&res);
     bignum_print(&res);
 
     return EXIT_SUCCESS;
